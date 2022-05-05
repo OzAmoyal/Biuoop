f903adabb2333b4882473e4ca7326c773bc645c9
OzAmoyal <93612510+OzAmoyal@users.noreply.github.com>
2022-05-05 19:12:14 +0300

    Add files via upload

---
+++ ass4/src/BaseExpression.java
@@ -0,0 +1,9 @@
+//oz amoyal 207231663
+
+/**
+ * @author ozamoyal
+ * abstract class repressenting an expression. for extension.
+ */
+public abstract class BaseExpression implements Expression {
+
+}
